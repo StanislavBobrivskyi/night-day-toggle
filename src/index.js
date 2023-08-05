a0dd6d996d65ffb41be6b0f9eabeca6a68deb10f
StanislavBobrivskyi <stanislavbobrivskyi@gmail.com>
2023-08-05 20:41:12 +0200

    customization

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ const SYNC = document.querySelector('#sync');
 
 const TOGGLE = () => {
   const IS_PRESSED = BUTTON.matches('[aria-pressed=true]');
-  if (SYNC.checked)
-    document.body.setAttribute('data-dark-mode', IS_PRESSED ? false : true);
+  //uncomment to be able to choose a topic via a checkbox
+  //   if (SYNC.checked)
+
+  document.body.setAttribute('data-dark-mode', IS_PRESSED ? false : true);
   BUTTON.setAttribute('aria-pressed', IS_PRESSED ? false : true);
 };
 
