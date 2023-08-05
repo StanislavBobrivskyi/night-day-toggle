ffa3e3c8aefcbc9e01463bdf732c83c5dd68c62f
StanislavBobrivskyi <121600218+StanislavBobrivskyi@users.noreply.github.com>
2023-08-05 18:25:29 +0000

    Deploying to gh-pages from @ StanislavBobrivskyi/night-day-toggle@178feaf832ce64058ee64f55e9fcfe498246d5dd 🚀

---
+++ index.4b3efc00.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("button"),t=document.querySelector("#sync");e.addEventListener("click",(function(){var r=e.matches("[aria-pressed=true]");t.checked&&document.body.setAttribute("data-dark-mode",!r),e.setAttribute("aria-pressed",!r)}))}();
+//# sourceMappingURL=index.4b3efc00.js.map
